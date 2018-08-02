7357e73a1a1d6bc502a056ef3914cbb825962611
Paul Schmidt <paul.schmidt@uni-hohenheim.de>
2018-08-02 20:03:33 +0200

    Lecture Notes Prof. Piepho

--- R code/06 crd repeated measures.R
+++ R code/06 crd repeated measures.R
@@ -2,25 +2,29 @@ rm(list=ls())
 setwd("D:/Hohenheim/R-SAS.Introductory.Courses/Datasets")
 
 library(data.table)
-dt <- fread("06 crd repeated measures.txt")
+dat <- fread("06 crd repeated measures.txt")
 
-# rename multiple columns
-names(dt) <- c("Soil", "Trt", "Rep", "Week", "Quant")
+# format multiple columns as.factor in one step
+makefactor <- names(dat)[1:4] # vector of columns that should be factor
+dat[, (makefactor) := lapply(.SD, as.factor), .SDcols=makefactor] # define all those columns as.factor
 
-dt$Soil <- as.factor(dt$Soil)
-dt$Trt  <- as.factor(dt$Trt)
-dt$Rep  <- as.factor(dt$Rep)
-dt$Week <- as.factor(dt$Week)
+# rename columns
+names(dat) <- c("Soil", "Trt", "Rep", "Week", "Quantity")
+
+###########################################################################
+# For more info on theory of repeated measures see ch. 7 of "Quantitative 
+# Methods in Biosciences" and ch. 6 in "Mixed models for metric data"
+###########################################################################
 
 # Option 1: Each week separately
 
-all.anovas <- list() #create an empty list to save multiple results in later
+all.anovas <- list() #create an empty list to save multiple results later
 
 # for-loop, goes through code multiple times.
 # "i" will be 1, 2 and 3 - so the loop runs three times.
 for(i in c(1,2,3)){
-  mod <- lm(data    = dt[Week==i],
-            formula = Quant ~ Trt + Soil + Trt:Soil)
+  mod <- lm(data    = dat[Week==i],
+            formula = Quantity ~ Trt + Soil + Trt:Soil)
   all.anovas[[i]] <- anova(mod)
 }
 
@@ -28,18 +32,18 @@ all.anovas # look at all three anova tables
 all.anovas[[1]] # look a first anova table
 
 # Option 2: Across weeks, accounting for correlation
-library(nlme)
+library(nlme) # another mixed model package
 
 # create a column that identifies a single plot
-dt$plotID <- as.factor(paste0(dt$Soil, dt$Trt, dt$Rep))
+dat$plotID <- as.factor(paste0(dat$Soil, dat$Trt, dat$Rep))
 # even nicer / more intuitive names:
-dt$plotID <- as.factor(paste0("S", dt$Soil, "-T", dt$Trt, "-R", dt$Rep))
+dat$plotID <- as.factor(paste0("S", dat$Soil, "-T", dat$Trt, "-R", dat$Rep))
 
-# mixed model with correlation structure
-mod <- lme(data   = dt,
-           fixed  = Quant ~ Trt + Soil + Week +
+# full mixed model with correlation structure
+mod <- lme(data   = dat,
+           fixed  = Quantity ~ Trt + Soil + Week +
                     Trt:Soil + Trt:Week + Soil:Week +
                     Trt:Soil:Week,
            random = ~ 1|plotID,
            correlation = corAR1())
-anova(mod)
+anova(mod) # Trt:Soil:Week is not significant - drop from model.
