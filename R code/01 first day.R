774420f53c1b927af98a47f676a5d02ed9546511
Paul Schmidt <paul.schmidt@uni-hohenheim.de>
2018-08-22 08:50:09 +0200

    Update after Day 1

--- R code/01 first day.R
+++ R code/01 first day.R
@@ -12,7 +12,7 @@ log(2) # the function "log" takes the logarithm of any number
 sqrt(a+b) #you can e.g. add a+b even inside a function
 
 
-# Names and order of function arguments
+### Names and order of function arguments
 # Some functions require multiple arguments. You can explicitly
 # address them with their names (e.g. "from=") or leave the name
 # out - IF you provide the arguments in the default order:
@@ -21,14 +21,16 @@ seq(2, 8, 0.5)            # works
 seq(2, 0.5, 8)            # does not work
 seq(from=2, by=0.5, to=8) # works
 
+?seq # get offline help for any function by writing a ? before it.
+
 ### vectors
 x <- c(1, 4, 5)
 y <- c("red", "blue")
 
-sqrt(x)
-mean(x)
-min(x)
-max(x)
+sqrt(x) # squareroot of each element in x
+mean(x) # mean of the values in x
+min(x)  # smalles value in x
+max(x)  # largest value in x
 
 ################
 # Handling Data
@@ -36,22 +38,19 @@ max(x)
 setwd("D:/Hohenheim/R-SAS.Introductory.Courses/Datasets") # set your working directory
 data <- read.delim("01 testfile.txt") # import txt file
 
-# format Species column as factor
-data$Species <- as.factor(data$Species)
+min(data$Weight)  # smallest weight value
+max(data$Weight)  # largest weight value
+mean(data$Weight) # mean value for weight
+round(mean(data$Weight), 3) # rounded mean value for weight
 
-mean(data$Weight)
+library(data.table) # activate data.table package from library (package must be installed first)
+# data.table is an optional additional package which lets you do multiple convenient things with your data. 
+DT <- data.table(data) # first, transform standard data to data.table format
 
-library(data.table) 
-# data.table is an optional additional package which lets you
-# do multiple convenient things with your data. 
-DT <- data.table(data) # transform standard data to data.table format
+# then you can do things like:
+DT[, mean(Weight)] # mean value for weight
+DT[, mean(Weight), by=Species] # mean weight per species
+DT[Count > 100, mean(Weight), by=Species] # mean weight per species for observations with Count>100
 
-# mean overall weight
-DT[, mean(Weight)]
-# mean weight per species
-DT[, mean(Weight), by=Species]
-# mean weight per species for observations with Count>100
-DT[Count > 100, mean(Weight), by=Species]
-
-DT[Count > 100, .(mean_w = mean(Weight),
-                  mean_c = mean(Count)), by=Species]
+# mean weight and mean count per species for observations with Count>100
+DT[Count > 100, .(mean_w = mean(Weight), mean_c = mean(Count)), by=Species] 
